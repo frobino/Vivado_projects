59832ce474e2ca2377a92ce309e7db4862e3e621
Francesco Robino <osso@T440p.bredbandsbolaget.se>
2014-12-29 11:21:42 +0100

    [CODE] re-structured folder to make tcl work
    Signed-off-by: Francesco Robino <osso@T440p.bredbandsbolaget.se>

--- my_first_microblaze/my_first_microblaze.tcl
+++ my_first_microblaze/my_first_microblaze.tcl
@@ -2,16 +2,19 @@
 set origin_dir "."
 
 # Set the directory path for the original project from where this script was exported
-set orig_proj_dir "[file normalize "$origin_dir"]"
+set orig_proj_dir "[file normalize "$origin_dir/my_first_microblaze"]"
+
+# Set project name
+set prj_name "my_first_noc"
 
 # Create project
-create_project my_first_microblaze .
+create_project $prj_name ./my_first_microblaze
 
 # Set the directory path for the new project
 set proj_dir [get_property directory [current_project]]
 
 # Set project properties
-set obj [get_projects my_first_microblaze]
+set obj [get_projects $prj_name]
 set_property "board_part" "em.avnet.com:zed:part0:1.2" $obj
 set_property "default_lib" "xil_defaultlib" $obj
 set_property "simulator_language" "Mixed" $obj
@@ -25,18 +28,20 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 # Set 'sources_1' fileset object
 set obj [get_filesets sources_1]
 set files [list \
- "[file normalize "$origin_dir/my_first_microblaze.srcs/sources_1/bd/microblaze_system_1/microblaze_system_1.bd"]"\
- "[file normalize "$origin_dir/my_first_microblaze.srcs/sources_1/bd/microblaze_system_1/hdl/microblaze_system_1_wrapper.vhd"]"\
+ "[file normalize "$origin_dir/bd_sources/microblaze_system_1.bd"]"\
+ "[file normalize "$origin_dir/hdl_sources/microblaze_system_1_wrapper.vhd"]"\
 ]
 add_files -norecurse -fileset $obj $files
 
 # Set 'sources_1' fileset file properties for remote files
-set file "$origin_dir/my_first_microblaze.srcs/sources_1/bd/microblaze_system_1/microblaze_system_1.bd"
+# set file "$origin_dir/my_first_microblaze/my_first_microblaze.srcs/sources_1/bd/microblaze_system_1/microblaze_system_1.bd"
+set file "$origin_dir/bd_sources/microblaze_system_1.bd"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property "generate_synth_checkpoint" "0" $file_obj
 
-set file "$origin_dir/my_first_microblaze.srcs/sources_1/bd/microblaze_system_1/hdl/microblaze_system_1_wrapper.vhd"
+# set file "$origin_dir/my_first_microblaze/my_first_microblaze.srcs/sources_1/bd/microblaze_system_1/hdl/microblaze_system_1_wrapper.vhd"
+set file "$origin_dir/hdl_sources/microblaze_system_1_wrapper.vhd"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property "file_type" "VHDL" $file_obj
@@ -58,16 +63,19 @@ if {[string equal [get_filesets -quiet constrs_1] ""]} {
 set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
-set file "[file normalize "$origin_dir/my_first_microblaze.srcs/constrs_1/new/constraint1.xdc"]"
+# set file "[file normalize "$origin_dir/my_first_microblaze/my_first_microblaze.srcs/constrs_1/new/constraint1.xdc"]"
+set file "[file normalize "$origin_dir/constrs_1/constraint1.xdc"]"
 set file_added [add_files -norecurse -fileset $obj $file]
-set file "$origin_dir/my_first_microblaze.srcs/constrs_1/new/constraint1.xdc"
+# set file "$origin_dir/my_first_microblaze/my_first_microblaze.srcs/constrs_1/new/constraint1.xdc"
+set file "$origin_dir/constrs_1/constraint1.xdc"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
 set_property "file_type" "XDC" $file_obj
 
 # Set 'constrs_1' fileset properties
 set obj [get_filesets constrs_1]
-set_property "target_constrs_file" "$orig_proj_dir/my_first_microblaze.srcs/constrs_1/new/constraint1.xdc" $obj
+# set_property "target_constrs_file" "$orig_proj_dir/my_first_microblaze.srcs/constrs_1/new/constraint1.xdc" $obj
+set_property "target_constrs_file" "$origin_dir/constrs_1/constraint1.xdc" $obj
 
 # Create 'sim_1' fileset (if not found)
 if {[string equal [get_filesets -quiet sim_1] ""]} {
